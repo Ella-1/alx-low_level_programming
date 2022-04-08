fbc56b99d0262f595c76b136383ff98f40b90534
Bata <es9897288@.com>
2022-04-08 07:01:02 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: Prints digit number of base ten from 0 to 10
+ * Return: 0
+ */
+int main (void)
+{
+	int c;
+
+	for (c = 0; c <=10; ++c)
+{
+	printf("%d\n", c);
+}
+	return (0);
+}
+
