a7c7da8706bf96b9654dc885d33b46892a0eb5b6
Bata <es9897288@.com>
2022-04-08 07:04:20 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,11 +5,11 @@
  * Description: Prints digit number of base ten from 0 to 10
  * Return: 0
  */
-int main (void)
+int main(void)
 {
 	int c;
 
-	for (c = 0; c <=10; ++c)
+	for (c = 0; c < 10; ++c)
 {
 	printf("%d\n", c);
 }
