56d0e08e0153f7b95bff61820d8c8b288a38223a
Bata <es9897288@.com>
2022-04-08 07:47:55 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print lowercase alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z')
+{
+	putchar('0' + -c);
+}
+	putchar('\n');
+	return (0);
+}
