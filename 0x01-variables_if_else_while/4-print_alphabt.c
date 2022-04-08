8b4eeafaf4597522ca3fa3b300836422bba3e6d6
Bata <es9897288@.com>
2022-04-08 06:35:38 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: prints the alphabet in lowercase excluding q and e
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	for (c = 'a'; c = 'z'; ++c) {
+	if ( c != 'q' && c != 'e')
+	putchar(c);
+}
+	putchar('\n');
+	return (0);
+}
