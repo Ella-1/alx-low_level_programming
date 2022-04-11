16579261cf2a46af03e9c7bc867055dd0accba95
Bata <es9897288@.com>
2022-04-11 03:51:48 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print all possible combination of two different digits,
+ * in ascending order, seperated by a comma followed by space.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i, j;
+	for (i = 0; i <9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		putchar((i % 10) + '0');
+		putchar((i % 10) + '0');
+
+		if (i == 8 && j == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
+	putchar('\n');
+	return (0);
+}
