e2a579cb8ae969a4b6c1f72bb9524f6f0e473016
Bata <es9897288@.com>
2022-04-11 04:43:01 -0700

    task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,7 +12,7 @@ int main(void)
 	{
 		for (j = i + 1; j < 9; j++)
 		{
-			for (k = j +1; j <10; K++)
+			for (k = j + 1; j < 10; k++)
 			{
 				putchar((i % 10) + '0');
 				putchar((j % 10) + '0');
@@ -20,6 +20,7 @@ int main(void)
 
 				if (i == 7 && j == 8 && k == 9)
 					continue;
+
 					putchar(',');
 					putchar(' ');
 			}
