bf28e1a95ee5b06332603347ce58f3f0ca7b8050
Bata <es9897288@.com>
2022-04-12 04:40:00 -0700

    task 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,24 @@
  *
  * Return: Always 0
  */
-void print_alphabet_x10(void)
+void  print_alphabet_x10(void)
 {
 
 	int i;
-	for (i = 0; i < 26; i++)
+	int count;
+
+	count = 0;
+	while (count < 10) 
 	{
-		_putchar('a' + i);
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
 	}
-		
+		count++;
 		_putchar('\n');
-		return (0);
+	}
+
+
+
 }
 
