6506191506f6c8c2d91e2d04b4c876b10706212a
Bata <es9897288@.com>
2022-04-12 04:33:20 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - prints alphabet, in lowercase followed by a new line
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	void print_alphabet_x10(void);
+
 	int i;
 	for (i = 0; i < 26; i++)
 	{
