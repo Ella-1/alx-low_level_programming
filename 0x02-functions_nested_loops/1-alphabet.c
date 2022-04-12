199a831bcd2dfdb5c6e7645fde9de9cb9979409b
Bata <es9897288@.com>
2022-04-12 03:05:01 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet in lowercase followed by a new line.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	int i;
+	void print_alphabet();
+	for (i = 0; i <= 26; i++)
+{
+	putchar('a' + c);
+}
+	putchar('\n');
+	return (0);
+}
