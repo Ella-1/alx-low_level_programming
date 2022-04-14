423049acbf986c99583cc3d10ad7fbdcea28543d
Bata <es9897288@.com>
2022-04-14 11:12:57 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-Int _putchar(char c);
-Int _isupper(int c);
-Int  _isdigit(int c);
-Int mul(int a, int b);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -10,4 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
