f2fa0aea77ac8f60e6fc64b2541bfbc1e03c8f01
Bata <es9897288@.com>
2022-04-14 11:34:20 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - function to print digit (0 through 9).
+ * @c: inpute value to check.
+ * Return: int.
+ */
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c<= 57)
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
