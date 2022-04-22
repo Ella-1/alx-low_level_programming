89f539f842fb794535fa54db665124852e17862e
Bata <es9897288@.com>
2022-04-22 04:53:24 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * _strncat - function that concates two strings.
+ * @dest: first string.
+ * @src: secound string.
+ * @n: number of bytes to use from src.
+ * Return: string.
+ */
+
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0, k = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[k] != '\0' && n > k)
+	{
+		dest[i] = src[k];
+		k++;
+		i++;
+	}
+	if (n > 0)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
