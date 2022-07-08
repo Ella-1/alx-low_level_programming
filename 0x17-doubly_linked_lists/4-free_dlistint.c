19c8cc1ab05d66dbebc3080e7caa7c143450b170
Bata <es9897288@.com>
2022-07-08 12:40:06 -0700

    group a

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * free_dlistint - Free a doubly linked list
+  * @head: The head of the doubly linked list
+  *
+  * Return: Nothing
+  */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head, *next = NULL;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+
